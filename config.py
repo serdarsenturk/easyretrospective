149c80574dde4f2f5eb0d5b6aaecbf90d7c63e96
Serdar <serdarsenturk@windowslive.con>
2021-10-31 01:12:32 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
 # Statement for enabling the development environment
 DEBUG = True
-
-import os
-
-# Define the database - we are working with
-# Postgresql for this example
-SQLALCHEMY_DATABASE_URI = os.environ['DB_CONNECTION_STRING']
