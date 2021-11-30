eaefedb84fbe13a280090b929c72cbb435e21752
Serdar <serdarsenturk@windowslive.con>
2021-11-30 17:35:24 +0300

    Re-name board's `capacity` and `password` without `board` prefixes

--- app/models/board.py
+++ app/models/board.py
@@ -5,7 +5,7 @@ class Board(db.Model):
     __tablename__ = 'boards'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    board_capacity = db.Column(db.Integer, nullable=False)
-    board_password = db.Column(db.Integer, nullable=False)
     team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    password = db.Column(db.String(15), nullable=False)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
