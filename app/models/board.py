36c3b0a822c14eefda259ff0270e660bbfa2533f
Serdar <serdarsenturk@windowslive.con>
2021-12-01 00:03:42 +0300

    Remove `capacity` and `password` properties

--- app/models/board.py
+++ app/models/board.py
@@ -5,6 +5,4 @@ class Board(db.Model):
     __tablename__ = 'boards'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    capacity = db.Column(db.Integer, nullable=False)
-    password = db.Column(db.String(15), nullable=False)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
