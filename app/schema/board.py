dad37088066f6387312ee43181a3736b6e6779f0
Serdar <serdarsenturk@windowslive.con>
2021-12-01 00:03:11 +0300

    Return `board_schema` for specific board  or `boards_schema` for return multiple boards

--- app/schema/board.py
+++ app/schema/board.py
@@ -4,3 +4,6 @@ from app import ma
 class BoardSchema(ma.SQLAlchemyAutoSchema):
     id = fields.Int()
     member_id = fields.Int()
+
+board_schema = BoardSchema()
+boards_schema = BoardSchema(many=True)
