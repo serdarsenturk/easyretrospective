e62803f4a1802d44bf882d354603b9ff0f260f89
Serdar <serdarsenturk@windowslive.con>
2021-11-30 17:35:38 +0300

    Remove `team_id` column

--- app/models/board.py
+++ app/models/board.py
@@ -5,7 +5,6 @@ class Board(db.Model):
     __tablename__ = 'boards'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
-    team_id = db.Column(db.Integer, db.ForeignKey('team.id'), nullable=False)
     capacity = db.Column(db.Integer, nullable=False)
     password = db.Column(db.String(15), nullable=False)
     member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
