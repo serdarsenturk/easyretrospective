a7faa47c46c141d767cda62977601d0b94820f0b
Serdar <serdarsenturk@windowslive.con>
2021-12-02 20:45:55 +0300

    Create code generator function

--- app/api/board.py
+++ app/api/board.py
@@ -8,3 +8,18 @@ from app.models.board import Board
 from app.schema.board import board_schema, boards_schema
 
 boards = Blueprint('boards', __name__, url_prefix='/api/v1/boards/')
+
+def generate_board_code(board):
+    try:
+        board.id = db.session.execute(Sequence("boards_id_seq"))
+        board.code = base62.encode(hash(('boards', board.id)), 8)[-8:]
+
+        db.session.add(board)
+        db.session.commit()
+
+        return board
+
+    except IntegrityError:
+        db.session.rollback()
+        return generate_board_code(board)
+
