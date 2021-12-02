bc3e238238c57cf9a974b9342780bc2a04e566fc
Serdar <serdarsenturk@windowslive.con>
2021-12-02 20:35:30 +0300

    Import flask_migrate and reqister to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_marshmallow import Marshmallow
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
@@ -10,7 +11,10 @@ ma = Marshmallow(app)
 
 # Configurations
 app.config.from_object(Config)
+
+#Define the database object which is imported
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 # Import a module / component using its blueprint handler variable (ex./url)
 from app.api.home import home
