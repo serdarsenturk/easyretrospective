733481cfca5f09d10b3c1df2b8ba1cdc68763fe1
Serdar <serdarsenturk@windowslive.con>
2021-12-02 20:35:06 +0300

    Remove member_id field

--- app/models/board.py
+++ app/models/board.py
@@ -1,9 +1,7 @@
 from app import db
 
-
 class Board(db.Model):
     __tablename__ = 'boards'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     code = db.Column(db.String(8), nullable=False, unique=True, index=True)
-    member_id = db.Column(db.Integer, db.ForeignKey('member.id'), nullable=False)
