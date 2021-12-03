b526cebeef1f240d0cbda96629a66899c248265a
Serdar <serdarsenturk@windowslive.con>
2021-12-03 18:33:00 +0300

    Add boards column to  provide relationship between member table

--- app/models/member.py
+++ app/models/member.py
@@ -6,3 +6,4 @@ class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(255), nullable=False)
+    boards = db.relationship('Board', backref='member', lazy=True)
