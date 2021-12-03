86c273707a4d0739138dd73e8d1626003b62a9ef
Serdar <serdarsenturk@windowslive.con>
2021-12-03 18:32:11 +0300

    Remove password Column

--- app/models/member.py
+++ app/models/member.py
@@ -6,4 +6,3 @@ class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     mail = db.Column(db.String(255), nullable=False)
-    password = db.Column(db.String(15), nullable=False)
