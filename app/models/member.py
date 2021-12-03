70bbccb65a5a0bd3b77caf3cce6853b4f50edbf4
Serdar <serdarsenturk@windowslive.con>
2021-12-03 18:31:51 +0300

    Remove table_args

--- app/models/member.py
+++ app/models/member.py
@@ -2,9 +2,7 @@ from app import db
 
 class Member(db.Model):
     __tablename__ = 'members'
-    __table_args__ = (
-        db.UniqueConstraint('membership_id', 'name', 'mail', name='unq_membership_id_mail'),
-    )
+
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
     mail = db.Column(db.String(255), nullable=False)
