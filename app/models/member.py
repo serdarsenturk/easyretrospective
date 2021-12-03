c4d6ebb380b8bdaecdd001418f99e37289e32228
Serdar <serdarsenturk@windowslive.con>
2021-12-03 18:32:26 +0300

    Re-name `mail` column to `email`

--- app/models/member.py
+++ app/models/member.py
@@ -5,4 +5,4 @@ class Member(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(50), nullable=False)
-    mail = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String(255), nullable=False)
