62096fff9e2fde2627be6077d4bbc4a4515c30f2
Serdar <serdarsenturk@windowslive.con>
2021-12-03 18:39:01 +0300

    Add `code` and `member_id` column to board model

--- app/models/board.py
+++ app/models/board.py
@@ -5,3 +5,4 @@ class Board(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     code = db.Column(db.String(8), nullable=False, unique=True, index=True)
+    member_id = db.Column(db.Integer, db.ForeignKey('members.id'), nullable=False)
