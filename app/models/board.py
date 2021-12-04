229e4675f3348382a664e3f470a99f9d74916912
Serdar <serdarsenturk@windowslive.con>
2021-12-04 03:15:02 +0300

    Add `member_id` column and `columns` relationships

--- app/models/board.py
+++ app/models/board.py
@@ -6,3 +6,4 @@ class Board(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     code = db.Column(db.String(8), nullable=False, unique=True, index=True)
     member_id = db.Column(db.Integer, db.ForeignKey('members.id'), nullable=False)
+    columns = db.relationship('Column', backref='column', lazy=True)
