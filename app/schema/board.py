7f01c91e8d1b8a9da0a9add7312558e28ddfa5dc
Serdar <serdarsenturk@windowslive.con>
2021-12-04 03:13:21 +0300

    Add columns nested fields to `BoardSchema` class

--- app/schema/board.py
+++ app/schema/board.py
@@ -1,10 +1,13 @@
 from marshmallow import fields
 from app import ma
+from app.schema.column import ColumnSchema
+
 
 class BoardSchema(ma.SQLAlchemyAutoSchema):
     id = fields.Int()
     code = fields.Str()
     member_id = fields.Int()
+    columns = fields.Nested(ColumnSchema, many=True)
 
 board_schema = BoardSchema()
 boards_schema = BoardSchema(many=True)
