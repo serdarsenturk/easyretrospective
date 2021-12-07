aecec1b2e23eb3596a1561848a01125e14ce1792
Serdar <serdarsenturk@windowslive.con>
2021-12-07 03:31:06 +0300

    Add `name` column to `Board` model

--- app/models/board.py
+++ app/models/board.py
@@ -4,6 +4,7 @@ class Board(db.Model):
     __tablename__ = 'boards'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = db.Column(db.String(15), nullable=True)
     code = db.Column(db.String(8), nullable=False, unique=True, index=True)
     member_id = db.Column(db.Integer, db.ForeignKey('members.id'), nullable=False)
     columns = db.relationship('Column', backref='column', lazy=True)
