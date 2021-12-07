e7971440dba2a58692463df5c7c8789f7324bad3
Serdar <serdarsenturk@windowslive.con>
2021-12-07 03:31:46 +0300

    Update `Column` models `name` property instead of `column_name``

--- app/api/column.py
+++ app/api/column.py
@@ -1,21 +1,24 @@
 from flask import Blueprint, request, jsonify
-from app import db
+from flask_cors import CORS
+
+from app import db, app
 from app.models.board import Board
 from app.models.column import Column
 from app.schema.column import column_schema
 
 columns = Blueprint('columns', __name__, url_prefix='/api/v1/members/<member_id>/boards/<code>/columns')
+CORS(columns, resources={r"/api/*": {"origins": app.config.get('CORS_ORIGINS')}})
 
 @columns.route('', methods=["POST"])
 def create_column(member_id, code):
-    column_name = request.json['column_name']
+    name = request.json['name']
     board = db.session.query(Board) \
         .filter(Board.code == code) \
         .filter(Board.member_id == member_id) \
         .first()
 
     board_id = board.id
-    new_column = Column(column_name = column_name, board_id=board_id)
+    new_column = Column(name = name, board_id=board_id)
 
     db.session.add(new_column)
     db.session.commit()
@@ -44,7 +47,7 @@ def modify_column_by_id(member_id, code, column_id):
         .filter(Column.id == column_id) \
         .first()
 
-    column.column_name = request.json['column_name']
+    column.name = request.json['name']
 
     db.session.commit()
 
