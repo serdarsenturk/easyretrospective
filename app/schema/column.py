1b1bbaa3096eb16d526e133b4b96127c97876a93
Serdar <serdarsenturk@windowslive.con>
2021-12-07 03:30:32 +0300

    Update `column_name` field to `name`

--- app/schema/column.py
+++ app/schema/column.py
@@ -5,7 +5,7 @@ from app.schema.card import CardSchema
 
 class ColumnSchema(ma.SQLAlchemyAutoSchema):
     id = fields.Int()
-    column_name = fields.Str()
+    name = fields.Str()
     board_id = fields.Int()
     cards = fields.Nested(CardSchema, many=True)
 
