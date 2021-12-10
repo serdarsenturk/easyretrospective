41b363b54ee8dd0a44bfed5aebdfca1ec1eb0403
Serdar <serdarsenturk@windowslive.con>
2021-12-10 23:03:11 +0300

    Add `team_id` Column to Board model

--- app/models/board.py
+++ app/models/board.py
@@ -7,4 +7,5 @@ class Board(db.Model):
     name = db.Column(db.String(15), nullable=True)
     code = db.Column(db.String(8), nullable=False, unique=True, index=True)
     member_id = db.Column(db.Integer, db.ForeignKey('members.id'), nullable=False)
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=True)
     columns = db.relationship('Column', backref='column', lazy=True)
