1341c33c368cc641803eae474d3e7cc404eafef3
Serdar <serdarsenturk@windowslive.con>
2021-12-13 07:58:55 +0300

    Add `date` column

--- app/models/board.py
+++ app/models/board.py
@@ -5,6 +5,7 @@ class Board(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False)
     name = db.Column(db.String(15), nullable=True)
+    date = db.Column(db.DateTime, nullable=False)
     code = db.Column(db.String(8), nullable=False, unique=True, index=True)
     member_id = db.Column(db.Integer, db.ForeignKey('members.id'), nullable=False)
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'), nullable=True)
