2a8d57ca9cdfbf74ffab7cf2ce239e95707f5257
Serdar <serdarsenturk@windowslive.con>
2022-02-27 23:38:42 +0300

    Add `team_id` field

--- app/schema/member_board.py
+++ app/schema/member_board.py
@@ -5,6 +5,7 @@ class MemberBoardSchema(ma.SQLAlchemyAutoSchema):
     name = fields.Str()
     code = fields.Str()
     date = fields.DateTime()
+    team_id = fields.Integer()
 
 member_board_schema = MemberBoardSchema()
 member_boards_schema = MemberBoardSchema(many=True)
