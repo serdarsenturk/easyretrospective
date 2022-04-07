cf8e644976e928fce8c796680d6a2b2c9d7ccb96
Serdar <serdarsenturk@windowslive.con>
2022-04-07 03:16:46 +0300

    Remove code and date fields

--- app/schema/team_board.py
+++ app/schema/team_board.py
@@ -3,8 +3,6 @@ from app import ma
 
 class TeamBoardSchema(ma.SQLAlchemyAutoSchema):
     name = fields.Str()
-    code = fields.Str()
-    date = fields.DateTime()
 
 team_board_schema = TeamBoardSchema()
 team_boards_schema = TeamBoardSchema(many=True)
