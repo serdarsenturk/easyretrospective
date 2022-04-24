7b6e1f9b166e0dc175f48245f189491e9e51c930
Serdar <serdarsenturk@windowslive.com>
2022-04-24 19:48:44 +0300

    Import `teams_schema`

--- app/api/board.py
+++ app/api/board.py
@@ -8,6 +8,7 @@ from datetime import datetime
 import base62
 from app.schema.board import board_schema
 from app.schema.member_board import member_boards_schema
+from app.schema.team import teams_schema
 from app.models.board import Board
 from app.models.column import Column
 from app.models.member import Member
