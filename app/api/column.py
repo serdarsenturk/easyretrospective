9610d15d488938ff282ddccb419d936e08056a1c
Serdar <serdarsenturk@windowslive.com>
2022-04-28 19:13:25 +0300

    Check request owner is same as member_id

--- app/api/column.py
+++ app/api/column.py
@@ -20,6 +20,11 @@ pusher = Pusher(
 
 @columns.route('', methods=["POST"])
 def create_column(member_id, code):
+    uid = request.headers.get('member_id')
+
+    if member_id != uid:
+        return 'Wrong UID, please verify your identity to complete this method', 401
+
     name = request.json['name']
 
     board = db.session.query(Board) \
@@ -43,6 +48,11 @@ def create_column(member_id, code):
 
 @columns.route('<column_id>', methods=['DELETE'])
 def delete_column_by_id(member_id, code, column_id):
+    uid = request.headers.get('member_id')
+
+    if member_id != uid:
+        return 'Wrong UID, please verify your identity to complete this method', 401
+
     column = db.session.query(Column) \
         .filter(Board.code == code ) \
         .filter(Board.member_id == member_id) \
@@ -62,6 +72,11 @@ def delete_column_by_id(member_id, code, column_id):
 
 @columns.route('<column_id>/name', methods=['PUT'])
 def modify_column_by_id(member_id, code, column_id):
+    uid = request.headers.get('member_id')
+
+    if member_id != uid:
+        return 'Wrong UID, please verify your identity to complete this method', 401
+
     column = db.session.query(Column) \
         .filter(Board.code == code ) \
         .filter(Board.member_id == member_id) \
