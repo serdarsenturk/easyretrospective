7b51c7c0eab2f933bc02ee8405689edf045fb2f1
Serdar <serdarsenturk@windowslive.com>
2022-05-07 02:27:34 +0300

    Create `cert` variable to pass `FIREBASE_ADMIN_CREDENTIAL` option

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,18 @@ ma = Marshmallow(app)
 
 # Configurations
 app.config.from_object(Config)
+cert = {
+    "type": "service_account",
+    "project_id": app.config.get("PROJECT_ID"),
+    "private_key_id": app.config.get("PRIVATE_KEY_ID"),
+    "private_key": app.config.get("PRIVATE_KEY"),
+    "client_email": app.config.get("CLIENT_EMAIL"),
+    "client_id": app.config.get("CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": app.config.get("CLIENT_X509_CERT_URL"),
+}
 
 #Define the database object which is imported
 db = SQLAlchemy(app)
