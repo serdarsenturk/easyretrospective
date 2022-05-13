a908f96087259f8fbc5b3574738272fc770bd996
Serdar <serdarsenturk@windowslive.com>
2022-05-13 22:22:02 +0300

    Create `session_login` method with `/api/v1/member/login` to verify member's id token and return session_cookie

---
+++ app/api/authenticate.py
@@ -0,0 +1,21 @@
+@authenticate.route('/api/v1/member/login', methods=['POST'])
+def session_login():
+    id_token = request.headers["authorization"]
+
+    try:
+        decoded_claims = auth.verify_id_token(id_token)
+        # Only process if the user signed in within the last 5 minutes.
+        if time.time() - decoded_claims['auth_time'] < 5 * 60:
+            expires_in = datetime.timedelta(days=5)
+            expires = datetime.datetime.now() + expires_in
+            session_cookie = auth.create_session_cookie(id_token, expires_in=expires_in)
+            response = jsonify({'status': 'success'})
+            response.set_cookie(
+                'session', session_cookie, expires=expires, httponly=True, secure=False)
+            return response, 200
+        return 'Recent sign in required', 201
+    except auth.InvalidIdTokenError:
+        return 'Invalid ID token', 401
+    except exceptions.FirebaseError:
+        return 'Failed to create a session cookie'
+
