02fc6fe50e3cd5ce29e5ae3df33c1c76c29863a3
Serdar <serdarsenturk@windowslive.com>
2022-05-24 01:33:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config:
     PUSHER_KEY = os.environ['PUSHER_KEY']
     PUSHER_SECRET = os.environ['PUSHER_SECRET']
     PROJECT_ID = os.environ['PROJECT_ID']
-    PRIVATE_KEY_ID = os.environ['PRIVATE_KEY_ID'].replace("\\n", "\n")
-    PRIVATE_KEY = os.environ['PRIVATE_KEY']
+    PRIVATE_KEY_ID = os.environ['PRIVATE_KEY_ID']
+    PRIVATE_KEY = os.environ['PRIVATE_KEY'].replace("\\n", "\n")
     CLIENT_EMAIL = os.environ['CLIENT_EMAIL']
     CLIENT_ID = os.environ['CLIENT_ID']
     CLIENT_X509_CERT_URL = os.environ['CLIENT_X509_CERT_URL']
